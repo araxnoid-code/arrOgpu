c4ce200d0362c118e2bdee2b7ce76b27999376a4
araxnoid <araxnoid0@gmail.com>
2025-11-17 23:38:38 +0700

    use this broadcasting version

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,4 @@ use std::time::SystemTime;
 
 use arr_o_gpu::ArrOgpuModule;
 
-fn main() {
-    let module = ArrOgpuModule::default();
-    let array = module.array_from_vector(&[], &[]).unwrap();
-
-    let broadcast = module.broadcasting(&array, &[]);
-}
+fn main() {}
