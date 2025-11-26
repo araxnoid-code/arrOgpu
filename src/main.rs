98dd9c76d781710e2fb03ea9c6815233c4ac32bf
araxnoid <araxnoid0@gmail.com>
2025-11-26 13:29:57 +0700

    + method new for array init

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
 use std::ops::{Range, RangeFull};
 
-use arr_o_gpu::FlatteTrait;
+use arr_o_gpu::{ArrOgpuModule, FlatteTrait};
 
 fn main() {
-    // let module = ArrOgpuModule::default();
+    let module = ArrOgpuModule::default();
 
-    // let data = vec![vec![1f32], vec![2f32], vec![3f32], vec![4f32]];
-    // let flat = data.flatten();
-    // println!("{:?}", flat)
-
-    // module.new(&[10.]);
-    // let array = ArangeArray::arange(10..20)
-    //     .map(|v| v)
-    //     .step_by(1)
-    //     .to_GpuArray_with_shape(&[5, 2], &module)
-    //     .unwrap();
+    let array = module
+        .new([[0., 0., 0.], [1., 1., 1.], [1., 1., 1.]])
+        .unwrap();
+    println!("{}", array);
 }
