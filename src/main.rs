d61f3f8790b562dbbe4dae3a400b6326418a339f
araxnoid <araxnoid0@gmail.com>
2025-12-07 02:55:10 +0700

    clean code to nightly version

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,4 @@ use std::{ ops::{ Range, RangeFull }, sync::mpsc, time::UNIX_EPOCH };
 use arr_o_gpu::{ ArangeArray, ArrOgpuModule, FlatteTrait, GpuArray, r };
 use ndarray::{ Array1, Array2 };
 
-fn main() {
-    let len = 6;
-    let data = (0..len)
-        .into_iter()
-        .map(|v| v as f32)
-        .collect::<Vec<f32>>();
-
-    let module = ArrOgpuModule::default();
-
-    let array_a = module.array_from_vector(&data, &[2, 3]).unwrap();
-    println!("{}", array_a);
-
-    let index = array_a.view().collect();
-    println!("{:?}", module.get_heap())
-}
+fn main() {}
