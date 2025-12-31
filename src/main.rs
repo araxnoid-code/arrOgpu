d1e17ac9f514d534d46db4faee1ee1d9e148b9e8
araxnoid <araxnoid0@gmail.com>
2025-12-31 23:53:14 +0700

    adding features in arrOgpu toml

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,8 @@
-fn main() {}
+use arr_o_gpu::ArrOgpuModule;
+
+fn main() {
+    let module = ArrOgpuModule::default();
+
+    let array = module.array_from_vector(&[1.0], &[1]).unwrap();
+    println!("{}", array);
+}
