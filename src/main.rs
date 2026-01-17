f32f4388189a45fbbc5c9ce46dc3a2f6e3baacb5
araxnoid <araxnoid0@gmail.com>
2026-01-17 21:07:13 +0700

    optimize the prototype of sum_axis

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,11 @@ fn main() {
     let stride = vec![12, 4, 1];
     let axis = vec![1, 2];
 
-    let mut out_shape = shape.clone();
+    let mut out_shape = shape
+        .iter()
+        .enumerate()
+        .map(|(i, s)| (*s, stride[i]))
+        .collect::<Vec<(u32, u32)>>();
     let mut in_axis_shape = vec![1; shape.len()];
     let mut sum_len = 1;
     for axis in axis.iter().rev() {
@@ -18,7 +22,12 @@ fn main() {
     let out_stride = out_shape
         .iter()
         .enumerate()
-        .map(|(i, _)| out_shape[i + 1..].iter().product::<u32>())
+        .map(|(i, _)| {
+            out_shape[i + 1..]
+                .iter()
+                .map(|(shape, _)| *shape)
+                .product::<u32>()
+        })
         .collect::<Vec<u32>>();
     let in_axis_stride = in_axis_shape
         .iter()
@@ -26,34 +35,21 @@ fn main() {
         .map(|(i, _)| in_axis_shape[i + 1..].iter().product::<u32>())
         .collect::<Vec<u32>>();
 
-    println!("{in_axis_shape:?}");
-    println!("{in_axis_stride:?}");
-
     gpu_virtualizer.running(16, 16, 1, |x, y, _| {
-        let total_x = out_shape.iter().product::<u32>();
+        let total_x = out_shape.iter().map(|(shape, _)| *shape).product::<u32>();
         if x < total_x && y < sum_len {
-            let mut is_axis = false;
-            let mut idx = 0;
-            print!("( ");
-            for (d, s) in shape.iter().enumerate() {
-                for d_axis in &axis {
-                    if *d_axis == d {
-                        is_axis = true;
-                        break;
-                    }
-                }
-
-                if is_axis {
-                    let permute = (y / in_axis_stride[d]) % in_axis_shape[d];
-                    print!(" {} ", permute);
-                } else {
-                    let permute = (x / out_stride[idx]) % out_shape[idx];
-                    idx += 1;
-                    print!(" {} ", permute)
-                }
-                is_axis = false;
+            let mut index = 0;
+            for d_axis in &axis {
+                let permute = (y / in_axis_stride[*d_axis]) % in_axis_shape[*d_axis];
+                index += permute * stride[*d_axis];
             }
-            print!(" )")
+
+            for (d, (out_shape, stride)) in out_shape.iter().enumerate() {
+                let permute = (x / out_stride[d]) % out_shape;
+                index += permute * stride;
+            }
+
+            print!("{} ", index);
         }
     });
 }
