db35b880e8b6e884a0277dc42bebc98715d64bac
araxnoid <araxnoid0@gmail.com>
2026-01-23 20:00:42 +0700

    version/0.2.0

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ fn main() {
     let module = ArrOgpuModule::default();
 
     let array = module
-        .array_from_vector(&[1., 2., 3., 4., 5., 6.], &[2, 3])
+        .array_from_vector(&[1.0, 2.0, 3.0, 4.0, 5.0, 6.0], &[2, 3])
         .unwrap();
 
-    let result = array.add(&1.).unwrap();
+    let result = array.add(&1.0).unwrap();
 
     println!("{}", result);
     // [
